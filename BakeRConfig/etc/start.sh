d3a0fc8cb066ca35de9927d577f619d70a827b2d
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2018-10-16 17:33:22 +0200

    updated robot model

--- BakeRConfig/etc/start.sh
+++ BakeRConfig/etc/start.sh
@@ -1,14 +1,3 @@
-source ~/.bashrc
-
 #miracenter BakeRConfig:etc/BakeR-application.xml -v MCFFile=BakeRConfig:etc/maps/Kenter/map.mcf,cleaningMap=BakeRConfig:etc/maps/Kenter/map-clean.xml,segmentationMap=BakeRConfig:etc/maps/Kenter/map-segmentation.xml,useNogo=true
 
-miracenter BakeRConfig:etc/BakeR-application.xml -v MCFFile=EnvironmentMaps:maps/AVH2/map.mcf,cleaningMap=BakeRConfig:etc/maps/AVH2/map-clean.xml,segmentationMap=BakeRConfig:etc/maps/AVH2/map-segmentation.xml,useNogo=true WaypointVisitor:etc/WaypointVisitor.xml &
-
-ssh demo@192.168.5.2 /localhome/demo/Desktop/start_baker.sh &
-
-sleep 15s
-
-miracd AutomaticaGUI
-while [ true ] ; do
-	python python/gui/app.py
-done;
+miracenter BakeRConfig:etc/BakeR-application.xml -v MCFFile=EnvironmentMaps:maps/AVH2/map.mcf,cleaningMap=BakeRConfig:etc/maps/AVH2/map-clean.xml,segmentationMap=BakeRConfig:etc/maps/AVH2/map-segmentation.xml,useNogo=true
